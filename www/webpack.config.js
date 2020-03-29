3ed8ec5f714f614de519d7383cd65666cda67109
Shane O Donovan <shane.odonovan@teamwork.com>
2020-03-29 15:48:19 +0100

    separate css, minify

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -1,17 +1,30 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-
 const dist = path.resolve(__dirname, "dist");
+const devMode = process.env.NODE_ENV !== 'production';
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
+const TerserJSPlugin = require("terser-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 const appConfig = {
   entry: "./js/main.js",
   devServer: {
     contentBase: dist
   },
+  optimization: {
+    minimizer: [
+      new TerserJSPlugin({}),
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: devMode ? "[name].css" : "[name].[hash].css",
+      chunkFilename: devMode ? "[id].css" : "[id].[hash].css"
     })
   ],
   resolve: {
@@ -24,9 +37,12 @@ const appConfig = {
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader"
+        ]
+      }
     ],
   }
 };
@@ -34,6 +50,11 @@ const appConfig = {
 const workerConfig = {
   entry: "./js/worker.js",
   target: "webworker",
+  optimization: {
+    minimizer: [
+      new TerserJSPlugin({})
+    ]
+  },
   plugins: [
     new WasmPackPlugin({
       crateDirectory: path.resolve(__dirname, "../botw-wasm")
